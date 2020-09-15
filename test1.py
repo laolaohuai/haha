ba831bf7e2098ef100c16d481d17fe9b12363671
laolaohuai <793973102@qq.com>
2020-09-15 14:58:45 +0800

    Add files via upload

--- test1.py
+++ test1.py
@@ -18,19 +18,36 @@ def test(path,ex,calc,para):
                                 pathlist.append(pathlist[index]+"\\"+i)
                 else:
                         if ex=="*" or ex==string.split(pathlist[index],".")[-1]:
-                                calc(pathlist[index],para)
+                                calc(pathlist[index],para,index)
                 index+=1
 
-def test2(path,para):
+def test2(path,para,index):
+        #输出文件名
 	print(path)
 
-def test3(path,para):
+def test3(path,para,index):
+        #输出文件内容
         f = open(path, 'r')
         txt = f.read()
         f.close()
         print(txt)
-        #f = open(path, 'w')
-        #f.write(txt)
-        #f.close()
 
-#test(os.getcwd(),"txt",test3,[])
+def test4(path,para,index):
+        #用正则表达式对文件中的内容进行替换
+        f = open(path, 'r')
+        txt = f.read()
+        f.close()
+        txt = re.sub(para[0], para[1], txt)
+        f = open(path, 'w')
+        f.write(txt)
+        f.close()
+
+def test5(path,para,index):
+        #重命名文件(非常危险,慎用)
+        new_name="\\".join(string.split(path,"\\")[:-1])+"\\"+para[0]+str(index)+"."+string.split(path,".")[-1]
+        print(path+">>"+new_name)
+        #os.rename(path,new_name)
+
+
+#test(os.getcwd(),"lua",test3,[])
+test("E:\\haha-master\\1","*",test4,[r'.',"a"])
